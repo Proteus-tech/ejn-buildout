8eeb956c6376f55857da91439d5077866ec9cff1
Giacomo Spettoli <giacomo.spettoli@gmail.com>
2016-03-31 22:09:49 +0200

    fix the skype image when sharing

---
+++ src/ejn.theme/ejn/theme/browser/social.py
@@ -0,0 +1,29 @@
+from plone.app.layout.viewlets.social import SocialTagsViewlet as BaseViewlet
+from plone.namedfile.utils import get_contenttype
+
+
+class SocialTagsViewlet(BaseViewlet):
+    def update(self):
+        image_field_name = 'image'
+        super(SocialTagsViewlet, self).update()
+        if hasattr(self.context, image_field_name):
+            ogimage = next(
+                (t for t in self.tags if t.get('property') == 'og:image'), None)
+            ogimagetype = next(
+                (t for t in self.tags if t.get('property') == 'og:image:type'), None)
+            image = next(
+                (t for t in self.tags if t.get('itemprop') == 'image'), None)
+
+            image_url = '{base_url}/@@images/{fieldname}/{imagesize}'.format(
+                base_url=self.context.absolute_url(),
+                fieldname=image_field_name,
+                imagesize='large')
+
+            mimetype = get_contenttype(self.context.image)
+
+            if ogimage:
+                ogimage['content'] = image_url
+            if ogimagetype:
+                ogimagetype['content'] = mimetype
+            if image:
+                image['content'] = image_url
