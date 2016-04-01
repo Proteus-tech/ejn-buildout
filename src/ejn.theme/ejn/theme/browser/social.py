3819951cf4f66ed275ca9a80a3ba91608c608205
Giacomo Spettoli <giacomo.spettoli@gmail.com>
2016-04-01 11:26:46 +0200

    fix social sharing

--- src/ejn.theme/ejn/theme/browser/social.py
+++ src/ejn.theme/ejn/theme/browser/social.py
@@ -3,31 +3,39 @@ from plone.namedfile.utils import get_contenttype
 
 
 class SocialTagsViewlet(BaseViewlet):
+    _image_field_name = 'image'
+    _image_size = 'large'
+
+    @property
+    def image_url(self):
+        return '{base_url}/@@images/{fieldname}/{imagesize}'.format(
+            base_url=self.context.absolute_url(),
+            fieldname=self._image_field_name,
+            imagesize=self._image_size)
+
+    def twitter_metatags(self):
+        for t in self.tags:
+            if t.get('name') == 'twitter:image':
+                t['content'] = self.image_url
+            elif t.get('name') == 'twitter:card':
+                t['content'] = 'summary_large_image'
+
+    def opengraph_metatags(self):
+        mimetype = get_contenttype(self.context.image)
+        for t in self.tags:
+            if t.get('property') == 'og:image':
+                t['content'] = self.image_url
+            elif t.get('property') == 'og:image:type':
+                t['content'] = mimetype
+
+    def generic_metatags(self):
+        for t in self.tags:
+            if t.get('itemprop') == 'image':
+                t['content'] = self.image_url
+
     def update(self):
-        image_field_name = 'image'
         super(SocialTagsViewlet, self).update()
-        if hasattr(self.context, image_field_name):
-            ogimage = next(
-                (t for t in self.tags if t.get('property') == 'og:image'), None)
-            ogimagetype = next(
-                (t for t in self.tags if t.get('property') == 'og:image:type'), None)
-            twitterimage = next(
-                (t for t in self.tags if t.get('property') == 'twitter:image'), None)
-            image = next(
-                (t for t in self.tags if t.get('itemprop') == 'image'), None)
-
-            image_url = '{base_url}/@@images/{fieldname}/{imagesize}'.format(
-                base_url=self.context.absolute_url(),
-                fieldname=image_field_name,
-                imagesize='large')
-
-            mimetype = get_contenttype(self.context.image)
-
-            if ogimage:
-                ogimage['content'] = image_url
-            if ogimagetype:
-                ogimagetype['content'] = mimetype
-            if image:
-                image['content'] = image_url
-            if twitterimage:
-                twitterimage['content'] = image_url
+        if hasattr(self.context, self._image_field_name):
+            self.twitter_metatags()
+            self.opengraph_metatags()
+            self.generic_metatags()
