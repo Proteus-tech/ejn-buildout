af76e9b8290e477e61e1b290398e011aeeee519e
Giacomo Spettoli <giacomo.spettoli@gmail.com>
2016-04-14 20:32:02 +0200

    fix and enable the event listner

--- src/ejn.types/ejn/types/profile_actions.py
+++ src/ejn.types/ejn/types/profile_actions.py
@@ -1,41 +1,41 @@
 from Products.CMFCore.utils import getToolByName
-from zope.app.component import hooks 
+from zope.component.hooks import getSite
 
-from zLOG import LOG, INFO
 
-def modify_access(object, event):
+def modify_access(obj, event):
     """Modify project WF state and permissions
     """
-    site = hooks.getSite()
-    workflowTool = getToolByName(site, "portal_workflow")
-    
-    bioSharing = object.getBioSharing()
-    proSharing = object.getProSharing()
-    contactSharing = object.getContactSharing()
-    
-    status = workflowTool.getStatusOf("plone_workflow", object)
-    
+    site = getSite()
+    wftool = getToolByName(site, "portal_workflow")
+
+    bioSharing = obj.getBioSharing()
+    proSharing = obj.getProSharing()
+    contactSharing = obj.getContactSharing()
+
+    wfinfo = wftool.getStatusOf('member_profile_workflow', obj)
+    if not wfinfo:
+        return
+    state = wfinfo['review_state']
+
     if bioSharing == 'Public':
-      if workflowTool.getInfoFor(object,'review_state') != 'published':
-        workflowTool.doActionFor(object, "published")
-        
+        if state != 'published':
+            wftool.doActionFor(obj, "published")
+
     elif bioSharing == 'Unverified EJN members':
-      if workflowTool.getInfoFor(object,'review_state') != 'visible':
-        workflowTool.doActionFor(object, "show")
-        
+        if state != 'visible':
+            wftool.doActionFor(obj, "show")
+
     elif bioSharing == 'Verified journalists':
-      if workflowTool.getInfoFor(object,'review_state') != 'private':
-        workflowTool.doActionFor(object, "hide")
-        object.manage_addLocalRoles('verified-journalists', ('Reader',))
-        object.reindexObjectSecurity()
-    else: #this case matches when the sharing permission is set to EJN-staff
-      if workflowTool.getInfoFor(object,'review_state') != 'private':
-        workflowTool.doActionFor(object, "hide")
-        object.manage_addLocalRoles('ejn-staff', ('Reader',))
-        object.reindexObjectSecurity()
+        if state != 'private':
+            wftool.doActionFor(obj, "hide")
+            obj.manage_addLocalRoles('verified-journalists', ('Reader',))
+            obj.reindexObjectSecurity()
+    else:  # this case matches when the sharing permission is set to EJN-staff
+        if state != 'private':
+            wftool.doActionFor(obj, "hide")
+            obj.manage_addLocalRoles('ejn-staff', ('Reader',))
+            obj.reindexObjectSecurity()
 
     if proSharing == 'Verified journalists' or contactSharing == 'Verified journalists':
-      object.manage_addLocalRoles('verified-journalists', ('Reader',))
-      object.reindexObjectSecurity()
-
-
+        obj.manage_addLocalRoles('verified-journalists', ('Reader',))
+        obj.reindexObjectSecurity()
