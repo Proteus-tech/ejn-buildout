d713ed6d3c77fa9c33f06b9b4b3b1f513f92638b
David Siedband <david@zentraal.com>
2016-05-11 23:28:38 +0200

    reference catalog approach for project team

---
+++ src/ejn.types/ejn/types/browser/get_project_team_uids.py
@@ -0,0 +1,27 @@
+from Products.CMFCore.utils import getToolByName
+from Products.Archetypes.config import REFERENCE_CATALOG
+
+from Products.Five.browser import BrowserView
+from Acquisition import aq_inner
+from zope.security import checkPermission
+
+
+class GetProjectTeamUIDsView(BrowserView):
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def __call__(self):
+
+      reference_catalog = getToolByName(self, REFERENCE_CATALOG)
+
+      relations = reference_catalog.getBackReferences(self.context,
+                                                      relationship="relatesTo")
+
+      team = []
+
+      for rel in relations:
+          team.append(rel.targetUID)
+
+      return team
