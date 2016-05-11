15613c79f432e96378167e56af5d9e06eecfd6db
David Siedband <david@zentraal.com>
2016-05-11 07:03:35 +0200

    testing out the UID approach on the reference_catalog

---
+++ src/ejn.types/ejn/types/browser/get_member_program_uids.py
@@ -0,0 +1,26 @@
+from Products.CMFCore.utils import getToolByName
+from Products.Archetypes.config import REFERENCE_CATALOG
+
+from Products.Five.browser import BrowserView
+from Acquisition import aq_inner
+from zope.security import checkPermission
+
+
+class GetMemberProgramUIDsView(BrowserView):
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def __call__(self):
+
+        reference_catalog = getToolByName(self, REFERENCE_CATALOG)
+
+        relations = reference_catalog.getBackReferences(self.context,
+                                                        relationship="relatesTo")
+
+        projects = []
+
+        for rel in relations:
+            projects.append(rel.UID())
+        return projects
