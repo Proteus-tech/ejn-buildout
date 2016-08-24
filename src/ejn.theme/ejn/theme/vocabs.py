f14aaaed09dab0777916003c2934093a232289f2
Giacomo Spettoli <giacomo.spettoli@gmail.com>
2016-08-24 08:56:35 +0000

    customer asked for Taiwan not to be listed as a province of China

--- src/ejn.theme/ejn/theme/vocabs.py
+++ src/ejn.theme/ejn/theme/vocabs.py
@@ -10,8 +10,14 @@ import pycountry
 class CountriesVocabulary(object):
     """Vocabulary factory for countries"""
     def __call__(self, context):
-            items = [SimpleTerm(x.alpha2.encode('utf-8'), x.name.encode('utf-8'))
-                     for x in pycountry.countries]
+            items = []
+            for country in pycountry.countries:
+                value = country.alpha2.encode('utf-8')
+                title = 'Taiwan' in country.name and 'Taiwan' or country.name
+                title = title.encode('utf-8')
+                items.append(SimpleTerm(value, title))
+#            items = [SimpleTerm(x.alpha2.encode('utf-8'), x.name.encode('utf-8'))
+#                     for x in countries]
             items = [SimpleTerm(None,'Select a country...')] + items
             return SimpleVocabulary(items)
 
