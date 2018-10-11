b7aa4ed9a8d453fba671dcb2096bb0ff00e9dba7
Mohammad Tareq Alam <tareq@zitelab.dk>
2018-10-11 14:25:33 +0600

    stories now sorted with creation date

--- src/ejn.types/ejn/types/Extensions/getProjectObjs.py
+++ src/ejn.types/ejn/types/Extensions/getProjectObjs.py
@@ -1,4 +1,6 @@
 from Products.CMFCore.utils import getToolByName
+from operator import itemgetter
+
 
 def getStoriesForProject(context):
 
@@ -9,11 +11,11 @@ def getStoriesForProject(context):
     # Plone 4.1: objects=True argument to fetch full objects, not just
     # index brains
     references = reference_catalog.getBackReferences(
-                        project.UID(),
-                        relationship="under program")
+        project.UID(),
+        relationship="under program")
     # Resolve Reference objects to full objects
     # Return a generator method which will yield all full objects
-    return [ ref.getSourceObject() for ref in references ]
+    return [ref.getSourceObject() for ref in references]
 
 
 def getPublishedStoriesForProject(context):
@@ -26,19 +28,24 @@ def getPublishedStoriesForProject(context):
     # Plone 4.1: objects=True argument to fetch full objects, not just
     # index brains
     references = reference_catalog.getBackReferences(
-                        project.UID(),
-                        relationship="under program")
+        project.UID(),
+        relationship="under program")
     # Resolve Reference objects to full objects
     # Return a generator method which will yield all full objects
-    # 
+    #
     results = []
+    unsorted_result = []
     for ref in references:
-      obj = ref.getSourceObject()
-      # status = portal_workflow.getStatusOf("featured_publication_workflow", obj)
-      status = portal_workflow.getInfoFor(obj, 'review_state')
-      if status in ['published','featured']:
-        results.append(obj)
+        obj = ref.getSourceObject()
+        # status = portal_workflow.getStatusOf("featured_publication_workflow", obj)
+        status = portal_workflow.getInfoFor(obj, 'review_state')
+        if status in ['published', 'featured']:
+            results.append(obj)
+            unsorted_result.append({'obj': obj, 'created': obj.created()})
+    sorted_result = sorted(unsorted_result, key=itemgetter('created'), reverse=True)
+    results = [x.get('obj') for x in sorted_result]
     return results
+
 
 def getChildrenForProject(context):
 
@@ -49,8 +56,8 @@ def getChildrenForProject(context):
     # Plone 4.1: objects=True argument to fetch full objects, not just
     # index brains
     references = reference_catalog.getBackReferences(
-                        project.UID(),
-                        relationship="parent program")
+        project.UID(),
+        relationship="parent program")
     # Resolve Reference objects to full objects
     # Return a generator method which will yield all full objects
-    return [ ref.getSourceObject() for ref in references ]
+    return [ref.getSourceObject() for ref in references]
